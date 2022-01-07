e007c7646e4851182cee3a6cbc9941bee7a5dd4c
ashhher <asher.han.xiao@gmail.com>
2022-01-07 17:30:19 +0800

    ch1

---
+++ study/TypeScript/ch1/test.ts
@@ -0,0 +1,12 @@
+var button = document.querySelector("button") as HTMLInputElement;
+var num1 = document.getElementById("num1") as HTMLInputElement;
+var num2 = document.getElementById("num2") as HTMLInputElement;
+
+function add(num1: number, num2: number) {
+    return num1 + num2;
+}
+
+button.addEventListener("click", () => {
+    console.log(add(+num1.value, +num2.value));
+
+})
