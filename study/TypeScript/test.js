a88427e289ae48563d6806c1a87ffdd9b9520de0
ashhher <asher.han.xiao@gmail.com>
2022-01-07 18:54:05 +0800

    ch3

--- study/TypeScript/test.js
+++ study/TypeScript/test.js
@@ -1,9 +1,10 @@
-var button = document.querySelector("button");
-var num1 = document.getElementById("num1");
-var num2 = document.getElementById("num2");
-function add(num1, num2) {
-    return num1 + num2;
-}
-button.addEventListener("click", function () {
-    console.log(add(+num1.value, +num2.value));
-});
+// var button = document.querySelector("button") as HTMLInputElement;
+// var num1 = document.getElementById("num1") as HTMLInputElement;
+// var num2 = document.getElementById("num2") as HTMLInputElement;
+// function add(num1: number, num2: number) {
+//     return num1 + num2;
+// }
+// button.addEventListener("click", () => {
+//     console.log(add(+num1.value, +num2.value));
+// })
+console.log('hello');
--- study/TypeScript/test.ts
+++ study/TypeScript/test.ts
@@ -1,12 +1,15 @@
-var button = document.querySelector("button") as HTMLInputElement;
-var num1 = document.getElementById("num1") as HTMLInputElement;
-var num2 = document.getElementById("num2") as HTMLInputElement;
+// var button = document.querySelector("button") as HTMLInputElement;
+// var num1 = document.getElementById("num1") as HTMLInputElement;
+// var num2 = document.getElementById("num2") as HTMLInputElement;
 
-function add(num1: number, num2: number) {
-    return num1 + num2;
-}
+// function add(num1: number, num2: number) {
+//     return num1 + num2;
+// }
 
-button.addEventListener("click", () => {
-    console.log(add(+num1.value, +num2.value));
+// button.addEventListener("click", () => {
+//     console.log(add(+num1.value, +num2.value));
 
-})
+// })
+
+console.log('hello');
+
