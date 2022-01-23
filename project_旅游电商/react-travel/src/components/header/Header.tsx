9c63ca729b51ba9f81762b6fbc15b727c783e02d
ashhher <asher.han.xiao@gmail.com>
2022-01-23 21:29:13 +0800

    react-redux

--- project_旅游电商/react-travel/src/components/header/Header.tsx
+++ project_旅游电商/react-travel/src/components/header/Header.tsx
@@ -4,26 +4,32 @@ import logo from '../../assets/logo.svg';
 import { Layout, Typography, Input, Menu, Button, Dropdown } from "antd";
 import { GlobalOutlined } from '@ant-design/icons';
 import { useNavigate, useLocation } from "react-router-dom";
-import store from "../../redux/store";
 import { useTranslation } from "react-i18next";
 import { addLanguageActionCreator, changeLanguageActionCreator } from "../../redux/language/languageActions";
+import { useSelector } from "../../redux/hooks"
+import { useDispatch } from "react-redux";
 
 
 export const Header: React.FC = () => {
     const navigate = useNavigate();
     const location = useLocation();
-    const storeState = store.getState();
     const { t } = useTranslation();
 
-    const [language, setLanguage] = useState<string>(storeState.language);
-    const [languageList, setLanguageList] = useState<any>(storeState.languageList);
+    const language = useSelector((state) => state.language);
+    const languageList = useSelector((state) => state.languageList);
+    const dispatch = useDispatch();
 
-    store.subscribe(() => {
-        const storeState = store.getState();
-        setLanguage(storeState.language);
-        setLanguageList(storeState.languageList);
-    });
+    // const [language, setLanguage] = useState<string>(storeState.language);
+    // const [languageList, setLanguageList] = useState<any>(storeState.languageList);
 
+    // 订阅store，此处因为使用react-redux 可省去
+    // store.subscribe(() => {
+    //     const storeState = store.getState();
+    //     setLanguage(storeState.language);
+    //     setLanguageList(storeState.languageList);
+    // });
+
+    // 跳转页面
     const toPage = (url: string) => {
         if (location.pathname === (url + '/'))
             window.location.reload();
@@ -31,14 +37,17 @@ export const Header: React.FC = () => {
             navigate(`${url}/`);
     };
 
+    // 语言点击事件
     const menuClickHandler = (e) => {
         if (e.key === 'new') {
             const code = languageList.length;
-            const action = addLanguageActionCreator("New Language " + code, "nl-" + code);
-            store.dispatch(action);
+            // const action = addLanguageActionCreator("New Language " + code, "nl-" + code);
+            // store.dispatch(action);
+            dispatch(addLanguageActionCreator("New Language " + code, "nl-" + code));
         } else {
-            const action = changeLanguageActionCreator(e.key);
-            store.dispatch(action);
+            // const action = changeLanguageActionCreator(e.key);
+            // store.dispatch(action);
+            dispatch(changeLanguageActionCreator(e.key));
         }
     };
 
@@ -87,21 +96,21 @@ export const Header: React.FC = () => {
             {/* menu-header */}
             <Menu mode='horizontal' className={styles['main-menu']}>
                 <Menu.Item key={1}>{t("header.home_page")}</Menu.Item>
-                <Menu.Item key={1}>{t("header.weekend")}</Menu.Item>
-                <Menu.Item key={2}>{t("header.group")}</Menu.Item>
-                <Menu.Item key={3}>{t("header.backpack")}</Menu.Item>
-                <Menu.Item key={4}>{t("header.private")}</Menu.Item>
-                <Menu.Item key={5}>{t("header.cruise")}</Menu.Item>
-                <Menu.Item key={6}>{t("header.hotel")}</Menu.Item>
-                <Menu.Item key={7}>{t("header.local")}</Menu.Item>
-                <Menu.Item key={8}>{t("header.theme")}</Menu.Item>
-                <Menu.Item key={9}>{t("header.custom")}</Menu.Item>
-                <Menu.Item key={10}>{t("header.study")}</Menu.Item>
-                <Menu.Item key={11}>{t("header.visa")}</Menu.Item>
-                <Menu.Item key={12}>{t("header.enterprise")}</Menu.Item>
-                <Menu.Item key={13}>{t("header.high_end")}</Menu.Item>
-                <Menu.Item key={14}>{t("header.outdoor")}</Menu.Item>
-                <Menu.Item key={15}>{t("header.insurance")}</Menu.Item>
+                <Menu.Item key={2}>{t("header.weekend")}</Menu.Item>
+                <Menu.Item key={3}>{t("header.group")}</Menu.Item>
+                <Menu.Item key={4}>{t("header.backpack")}</Menu.Item>
+                <Menu.Item key={5}>{t("header.private")}</Menu.Item>
+                <Menu.Item key={6}>{t("header.cruise")}</Menu.Item>
+                <Menu.Item key={7}>{t("header.hotel")}</Menu.Item>
+                <Menu.Item key={8}>{t("header.local")}</Menu.Item>
+                <Menu.Item key={9}>{t("header.theme")}</Menu.Item>
+                <Menu.Item key={10}>{t("header.custom")}</Menu.Item>
+                <Menu.Item key={11}>{t("header.study")}</Menu.Item>
+                <Menu.Item key={12}>{t("header.visa")}</Menu.Item>
+                <Menu.Item key={13}>{t("header.enterprise")}</Menu.Item>
+                <Menu.Item key={14}>{t("header.high_end")}</Menu.Item>
+                <Menu.Item key={15}>{t("header.outdoor")}</Menu.Item>
+                <Menu.Item key={16}>{t("header.insurance")}</Menu.Item>
             </Menu>
         </div>
     )
